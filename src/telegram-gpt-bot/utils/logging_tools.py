a7f9bb57991b7168033e78d28e8f9cbce53b1145
zeusina <kachusov_k@outlook.com>
2023-05-29 16:48:46 +0300

    Added get_logger function to logging_tools

--- src/telegram-gpt-bot/utils/logging_tools.py
+++ src/telegram-gpt-bot/utils/logging_tools.py
@@ -6,4 +6,16 @@ def configurate_logging():
     init()
     logging.basicConfig(level=logging.DEBUG,
                         format=Fore.BLACK + "%(asctime)s" + Fore.CYAN + " %(levelname)s      "
-                        + Fore.RED + "%(funcName)s" + " %(message)s")
+                               + Fore.RED + "%(funcName)s" + " %(message)s")
+
+
+def get_logger(name: str = __name__, level: int = logging.DEBUG) -> logging.Logger:
+    logger = logging.getLogger(name)
+    logger_stream_handler = logging.StreamHandler()
+    logger_formatter = logging.Formatter(Fore.BLACK + "%(asctime)s" + Fore.CYAN + " %(levelname)s      "
+                                         + Fore.RED + "%(funcName)s" + " %(message)s")
+    logger_stream_handler.setFormatter(logger_formatter)
+    logger.addHandler(logger_stream_handler)
+    logger.setLevel(level)
+    return logger
+
