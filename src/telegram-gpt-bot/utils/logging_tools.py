d96fa0f66d840bb7bf400f8d16c55840a03bac9c
zeusina <kachusov_k@outlook.com>
2023-05-29 17:28:47 +0300

    Updated logging format to identify loggers

--- src/telegram-gpt-bot/utils/logging_tools.py
+++ src/telegram-gpt-bot/utils/logging_tools.py
@@ -6,7 +6,7 @@ def configurate_logging():
     init()
     logging.basicConfig(level=logging.DEBUG,
                         format=Fore.BLACK + "%(asctime)s" + Fore.CYAN + " %(levelname)s      "
-                               + Fore.RED + "%(funcName)s" + " %(message)s")
+                               + Fore.RED + "%(name)s-%(funcName)s" + " %(message)s")
 
 
 def get_logger(name: str = __name__, level: int = logging.DEBUG) -> logging.Logger:
