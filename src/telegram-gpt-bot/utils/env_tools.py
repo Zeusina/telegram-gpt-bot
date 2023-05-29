6606750c0694c84d5b977ad586abc9145a327e7e
zeusina <kachusov_k@outlook.com>
2023-05-29 18:07:16 +0300

    Added file check in env_tools

--- src/telegram-gpt-bot/utils/env_tools.py
+++ src/telegram-gpt-bot/utils/env_tools.py
@@ -1,11 +1,15 @@
 import sys
+import os
 import pathlib
 import dotenv
-import logging
+from logging_tools import get_logger
 
 
 def get_env_from_file():
-    dotenv_path=pathlib.Path(sys.path[0]).absolute().parent.parent.joinpath(".env")
-    logging.debug("Path for .env file: " + str(dotenv_path))
-    dotenv.load_dotenv(dotenv_path=dotenv_path)
-
+    logger = get_logger("env_tools")
+    dotenv_path = pathlib.Path(sys.path[0]).absolute().parent.parent.joinpath(".env")
+    if os.path.exists(dotenv_path):
+        logger.debug("Loading dotenv file: " + str(dotenv_path))
+        dotenv.load_dotenv(dotenv_path=dotenv_path)
+    else:
+        logger.error("Dotenv file not found in:" + str(dotenv_path))
