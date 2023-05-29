7a583548ad625a9ca94ebb5b9f64a66579b8a972
zeusina <kachusov_k@outlook.com>
2023-05-29 12:47:02 +0300

    Added bot start in main

--- src/telegram-gpt-bot/__main__.py
+++ src/telegram-gpt-bot/__main__.py
@@ -1,10 +1,15 @@
 from utils import get_env_from_file, configurate_logging
+import asyncio
+from telegram import bot
 
 
-def main():
+async def main():
     configurate_logging.configurate_logging()
     get_env_from_file.get_env_from_file()
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
+    telegram_stuff = asyncio.run(bot.setup_bot())
+    asyncio.run(bot.start_polling(telegram_stuff))
+
