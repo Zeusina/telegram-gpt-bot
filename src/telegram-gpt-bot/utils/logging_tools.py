1a1fab845fd3977f911a9bf8c83117500c54fdc8
zeusina <kachusov_k@outlook.com>
2023-05-29 17:23:48 +0300

    Fixed get_logging creation

--- src/telegram-gpt-bot/utils/logging_tools.py
+++ src/telegram-gpt-bot/utils/logging_tools.py
@@ -11,11 +11,6 @@ def configurate_logging():
 
 def get_logger(name: str = __name__, level: int = logging.DEBUG) -> logging.Logger:
     logger = logging.getLogger(name)
-    logger_stream_handler = logging.StreamHandler()
-    logger_formatter = logging.Formatter(Fore.BLACK + "%(asctime)s" + Fore.CYAN + " %(levelname)s      "
-                                         + Fore.RED + "%(funcName)s" + " %(message)s")
-    logger_stream_handler.setFormatter(logger_formatter)
-    logger.addHandler(logger_stream_handler)
     logger.setLevel(level)
     return logger
 
