488d7c907e1a54e8b3c53957de4117a2227ecdf2
Zeusina <kachusov_k@outlook.com>
2023-05-30 14:34:37 +0300

    Changed logging library to own function get_logger

--- src/telegram_gpt_bot/telegram/handlers/commands/start_command.py
+++ src/telegram_gpt_bot/telegram/handlers/commands/start_command.py
@@ -1,10 +1,9 @@
 from aiogram.types import Message
 from src.telegram_gpt_bot.utils.logging_tools import get_logger
-import logging
 
 
 async def start_command(message: Message):
-    logger = logging.getLogger("telegram/handlers/commands/start_command")
+    logger = get_logger("telegram/handlers/commands/start_command")
     await message.answer(f"Привет, {message.from_user.full_name}!")
     logger.info("Bot started by: " + message.chat.full_name)
 
