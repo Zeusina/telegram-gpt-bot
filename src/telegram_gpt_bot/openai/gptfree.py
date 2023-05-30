0567356215fe60fcc887c54346df97c686f610bd
Zeusina <kachusov_k@outlook.com>
2023-05-30 20:48:58 +0300

    Renamed function free_GPT to request_from_gpt & renamed values & added answer return

--- src/telegram_gpt_bot/openai/gptfree.py
+++ src/telegram_gpt_bot/openai/gptfree.py
@@ -3,12 +3,10 @@ from gpt4free import Provider
 import logging
 
 
-
-def GPT_free(user):
+async def request_to_gpt(promt: str) -> str:
     logging.basicConfig(level=logging.DEBUG)
 
     # usage You
     response = bytes(
-        gpt4free.Completion.create(Provider.You, prompt=user), "utf-8").decode('unicode-escape')
-    print(response)
-
+        gpt4free.Completion.create(Provider.You, prompt=promt), "utf-8").decode('unicode-escape')
+    return response
