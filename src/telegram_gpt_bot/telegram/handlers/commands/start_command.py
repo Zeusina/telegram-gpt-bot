c4923c63360a5068ded82dcd79dae6898de263c8
zeusina <kachusov_k@outlook.com>
2023-05-30 15:34:42 +0300

    Replacead absolute import relative

--- src/telegram_gpt_bot/telegram/handlers/commands/start_command.py
+++ src/telegram_gpt_bot/telegram/handlers/commands/start_command.py
@@ -1,5 +1,5 @@
 from aiogram.types import Message
-from src.telegram_gpt_bot.utils.logging_tools import get_logger
+from ....utils.logging_tools import get_logger
 
 
 async def start_command(message: Message):
