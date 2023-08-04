e403961e66c4384d56d08ae90313ba23e2e446ae
Zeusina <kachusov_k@outlook.com>
2023-08-04 11:01:22 +0300

    Now use new gpt4free & ChatGPTAI provider

--- src/telegram_gpt_bot/openai/gptfree.py
+++ src/telegram_gpt_bot/openai/gptfree.py
@@ -1,14 +1,11 @@
 
-from src.telegram_gpt_bot.openai.gpt4free import Provider
-
-from src.telegram_gpt_bot.openai import gpt4free
+import g4f
 from src.telegram_gpt_bot.utils.logging_tools import get_logger
 
 
 async def request_to_gpt(promt: str) -> str:
     logger = get_logger("openai/gptfree")
 
-    # usage You
-    response = bytes(
-        gpt4free.Completion.create(Provider.You, prompt=promt), "utf-8").decode('unicode-escape')
+    response = g4f.ChatCompletion.create(model='gpt-3.5-turbo', messages=[
+            {"role": "user", "content": promt}], stream=False, provider=g4f.Provider.ChatgptAi)
     return response
